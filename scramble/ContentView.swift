a6e64aa76b38bbef2f27e925e7f30688b8d26a08
Makstarr <maksi112/@>
2020-02-10 21:47:18 +0300

    Initial Commit

---
+++ scramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scramble
+//
+//  Created by Максим on 10.02.2020.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
